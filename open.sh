7d9d56f8e5cd15c40e6e119c7b4241a5723769b4
ashdemon <154126473+ashdemon@users.noreply.github.com>
2023-12-21 10:30:53 +0200

    Create open.sh

---
+++ open.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#ash-demon
+#WhatsApp +201027938276
+#facebook https://www.facebook.com/ash801demon/
+#instagram https://www.instagram.com/ashx801/
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+clear
+echo -e $green "-------------------------------------------------------"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                 $red open.sh O.S GLITCH    $green             -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                  $blue ASH-DEMON     $green                    -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-------------------------------------------------------"
+sleep 0.5
+
+sleep 0.5
+echo ""
+echo -e $red     " "
+echo
+echo -e $green   "1- KAILL LINUX "
+echo
+echo -e $green   "2- ubuntu.sh"
+echo
+echo -e $green   "3- parrot"
+echo
+echo -e $green   "4-Debian"
+echo
+echo -e $green   "0- Exit"
+read use
+if [ $use = 2 ]
+then
+./startubuntu.sh
+fi
+if [ $use = 2 ]
+then
+./start-kali.sh
+fi
+if [ $use = 4 ]
+then
+ debian
+fi
+if [ $use = 3 ]
+then
+./startparrot.sh
+fi
+if [ $use = 0 ]
+then
+  exti
+  exti
+fi
