9bf9a25be545d5627d2c2ddbfb4c7806e2deb98e
ashdemon <154126473+ashdemon@users.noreply.github.com>
2023-12-22 09:38:17 +0200

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,26 @@
+echo -e $green "-------------------------------------------------------"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                 $red ABOUT TOOL GLITCH     $green             -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                  $blue ASH-DEMON     $green                    -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-------------------------------------------------------"
+sleep 0.5
+echo -e $green   "1- KAILL LINUX "
+echo
+echo -e $green   "#WhatsApp +201027938276"
+echo
+echo -e $green   "#facebook https://www.facebook.com/ash801demon/"
+echo
+echo -e $green   "#instagram https://www.instagram.com/ashx801/"
+echo
+echo -e $green   "5-about "
+echo
